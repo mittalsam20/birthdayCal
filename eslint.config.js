e262477d63ce9c2aae05f481c96cacf9ce0acced
WARMACHINE\sammi <sam.mittal44@gmail.com>
2025-09-24 01:20:40 +0530

    chore: init esLint and prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,29 +1,53 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import { defineConfig, globalIgnores } from "eslint/config";
+
+// plugins: { react },
+// import react from "eslint-plugin-react";
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(["dist"]),
   {
-    files: ['**/*.{js,jsx}'],
+    files: ["**/*.{js,jsx}"],
     extends: [
       js.configs.recommended,
-      reactHooks.configs['recommended-latest'],
+      reactHooks.configs["recommended-latest"],
       reactRefresh.configs.vite,
     ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
       parserOptions: {
-        ecmaVersion: 'latest',
+        ecmaVersion: "latest",
         ecmaFeatures: { jsx: true },
-        sourceType: 'module',
+        sourceType: "module",
       },
     },
     rules: {
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      "no-unused-vars": ["error", { varsIgnorePattern: "^[A-Z_]" }],
+      ...js.configs.recommended.rules,
+      ...react.configs.recommended.rules,
+      ...react.configs["jsx-runtime"].rules,
+      ...reactHooks.configs.recommended.rules,
+      "prefer-const": "warn",
+      "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+      "react/prop-types": "off",
+      "react/display-name": "warn",
+      "react/jsx-no-undef": "error",
+      "react/no-string-refs": "error",
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
+      "no-empty": "warn",
+      "no-undef": "error",
+      "no-unused-vars": "warn",
+      "no-useless-catch": "warn",
+      "no-useless-escape": "warn",
+      "no-prototype-builtins": "warn",
+      "no-extra-boolean-cast": "warn",
+      "no-async-promise-executor": "warn",
+      "no-console": ["warn", { allow: ["warn", "error"] }],
     },
   },
-])
+]);
