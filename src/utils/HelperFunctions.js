dfffe5fc2603d11b9250945acd95699ccb40b7bf
WARMACHINE\sammi <sam.mittal44@gmail.com>
2025-09-25 15:51:39 +0530

    refactor: remove redundant css

--- src/utils/HelperFunctions.js
+++ src/utils/HelperFunctions.js
@@ -1,22 +1,4 @@
-const colors = [
-  "#545D79",
-  "#8AB721",
-  "#C77D99",
-  "#78CAE3 ",
-  "#E64A33",
-  "#4A90E2", // Light blue
-  "#7B68EE", // Medium blue
-  "#87CEEB", // Sky blue
-  "#FFB6C1", // Light pink
-  "#98FB98", // Light green
-  "#F0E68C", // Khaki
-  "#FFA07A", // Light salmon
-  "#DDA0DD", // Plum
-  "#20B2AA", // Light sea green
-  "#FF6347", // Tomato
-  "#9370DB", // Medium purple
-  "#32CD32", // Lime green
-];
+import { TILE_COLORS } from "@constants/colorConstants";
 
 export const getNameInitials = ({ fullName = "" }) => {
   const names = fullName.split(" ");
@@ -32,5 +14,5 @@ export const getColorFromName = ({ name = "" }) => {
   for (let i = 0; i < name.length; i++) {
     hash = name.charCodeAt(i) + ((hash << 5) - hash);
   }
-  return colors[Math.abs(hash) % colors.length];
+  return TILE_COLORS[Math.abs(hash) % TILE_COLORS.length];
 };
